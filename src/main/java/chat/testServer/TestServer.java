e063dc1c8b9bd52fe75604cc049c9bc666d44a79
marctud <scorpioncsruler@yahoo.com>
2015-06-07 15:14:18 +0300

    testServer created

---
+++ src/main/java/chat/testServer/TestServer.java
@@ -0,0 +1,38 @@
+package chat.testServer;
+
+import java.io.IOException;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/test")
+public class TestServer {
+
+	@OnOpen
+	public void onOpen(Session session) {
+		System.out.println(session.getId() + " has opened a connection");
+		try {
+			session.getBasicRemote().sendText("Connection Established");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@OnMessage
+    public void onMessage(String message, Session session){
+        System.out.println("Message from " + session.getId() + ": " + message);
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+	
+	@OnClose
+    public void onClose(Session session){
+        System.out.println("Session " +session.getId()+" has ended");
+    }
+}
